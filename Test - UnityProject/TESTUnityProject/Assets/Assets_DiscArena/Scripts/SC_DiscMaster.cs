bdf515ac2fcf2ce78237547a1a5a60d1e9b3d20e
raigetsu <bruneauk1@gmail.com>
2022-06-16 20:50:05 +0200

    Prediction point display
    Make prediction visually with point
    if not playing display a debug line to see the prediction

--- Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/SC_DiscMaster.cs
+++ Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/SC_DiscMaster.cs
@@ -5,41 +5,16 @@ using UnityEngine;
 public class SC_DiscMaster : MonoBehaviour
 {
     [SerializeField] public Rigidbody rb = null;
-    [SerializeField] private LayerMask GroundLayer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        LaunchDisc(new Vector3(1f, 0f, -1f), 200f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        print("<color=green>" + rb.velocity+ "</color>");
-    }
 
     /*
      * Add Force to disc in the given Direction
     */
-    void LaunchDisc(Vector3 Direction, float Force = 1f)
+    public void LaunchDisc(Vector3 pDirection, float pForce = 1f)
     {
-        Direction.x *= Force;
-        Direction.y *= Force;
-        Direction.z *= Force;
+        pDirection.x *= pForce;
+        pDirection.y = 0f;
+        pDirection.z *= pForce;
 
-        rb.velocity = new Vector3(4, 0f,-4);
+        rb.velocity = pDirection;
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer != GroundLayer.value)
-        {
-
-        print(collision.gameObject.layer);
-        print("<color=red>" + rb.velocity+ "</color>");
-        }
-    }
-
- 
 }
