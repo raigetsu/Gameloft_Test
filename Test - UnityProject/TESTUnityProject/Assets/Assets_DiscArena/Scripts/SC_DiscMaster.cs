47da5a4e18a1b43b7cd43d1f467a74dc44fcb073
raigetsu <bruneauk1@gmail.com>
2022-06-16 22:21:04 +0200

    Add Attack / MoveSpeed to disc and Add disc changement in GameManager

--- Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/SC_DiscMaster.cs
+++ Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/SC_DiscMaster.cs
@@ -1,10 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class SC_DiscMaster : MonoBehaviour
 {
-    [SerializeField] public Rigidbody rb = null;
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private LayerMask GroundLayer = 0;
+
+    [Header("Data")]
+    [SerializeField] private int attack = 0;
+    [SerializeField] private float moveSpeed = 0f;
+
+    [Header("EVENT")]
+    [SerializeField] private UnityEvent OnMovementStop = new UnityEvent();
+    [SerializeField] private UnityEvent OnHit = new UnityEvent();
+
+
+    public int Attack { get => attack; }
+    public float MoveSpeed { get => moveSpeed; }
+
+    private bool IsInMovement = false;
+
+
+    private void Start()
+    {
+        OnMovementStop.AddListener(() => 
+        {
+            rb.velocity = Vector3.zero;
+            IsInMovement = false;
+        });
+    }
+
+    private void Update()
+    {
+        if (IsInMovement)
+        {
+            if (rb.velocity.magnitude <= 0.1f)
+            {
+                OnMovementStop?.Invoke();
+             
+            }
+        }
+    }
 
     /*
      * Add Force to disc in the given Direction
@@ -16,5 +53,14 @@ public class SC_DiscMaster : MonoBehaviour
         pDirection.z *= pForce;
 
         rb.velocity = pDirection;
+        IsInMovement = true;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(((1 << collision.gameObject.layer) & GroundLayer.value) != 0)
+        {
+            OnHit?.Invoke();
+        }
     }
 }
