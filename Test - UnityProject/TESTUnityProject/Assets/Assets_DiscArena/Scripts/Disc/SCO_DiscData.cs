d0aa7d2ad3d06d45ceefb5cc0a941b0127be9dc9
raigetsu <bruneauk1@gmail.com>
2022-06-18 13:58:50 +0200

    add move / rotation script + save to building

--- Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/Disc/SCO_DiscData.cs
+++ Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/Disc/SCO_DiscData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "DiscData",menuName = "Create DiscData")]
+[CreateAssetMenu(fileName = "DiscData", menuName = "Create DiscData")]
 public class SCO_DiscData : ScriptableObject
 {
     [Header("KEY")]
@@ -17,10 +17,16 @@ public class SCO_DiscData : ScriptableObject
     [SerializeField] private Sprite iconTexture = null;
     [SerializeField, Tooltip("Set -1 for unlimited use")] private int usableCount = -1;
 
+    [Header("INFORMATION")]
+    [SerializeField] private string discName = "CLASSIC";
+    [SerializeField, TextArea] private string discInformation = "Information";
+
     public SC_Rank.ERank UnlockedRank { get => unlockedRank; }
     public int UnlockedRankLevel { get => unlockedRankLevel; }
     public GameObject Prefab { get => prefab; }
     public Sprite IconTexture { get => iconTexture; }
     public int UsableCount { get => usableCount; }
     public string AddressablesKey { get => addressablesKey; }
+    public string DiscName { get => discName; }
+    public string DiscInformation { get => discInformation; }
 }
---
+++ Test - UnityProject/TESTUnityProject/Assets/Assets_DiscArena/Scripts/SC_MoveObject.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MoveObject : MonoBehaviour
+{
+    [System.Serializable]
+    public class SaveMoveObject
+    {
+        public Vector3[] checkpoint;
+        public float speed = 0f;
+        public bool pingPong = false;
+    }
+
+    [SerializeField] private Vector3[] checkpoint;
+    [SerializeField] private float speed = 0f;
+
+    [SerializeField] private bool pingPong = true;
+
+    private Vector3 direction = Vector3.zero;
+    private int posIndex = 1;
+    private bool update = true;
+    private bool playReverse = false;
+
+    private void Update()
+    {
+        if (update)
+        {
+            gameObject.transform.position += direction * speed * Time.deltaTime;
+            if (Vector3.Distance(gameObject.transform.position, checkpoint[posIndex]) <= 0.2f)
+            {
+                IsArriveAtDestination();
+            }
+        }
+    }
+
+    private void IsArriveAtDestination()
+    {
+        int startIndex = posIndex;
+        posIndex += playReverse ? -1 : 1;
+
+        if ((posIndex >= checkpoint.Length && playReverse == false) ||
+            (posIndex < 0 && playReverse == true))
+        {
+            if (pingPong)
+            {
+                playReverse = !playReverse;
+                posIndex = playReverse ? checkpoint.Length - 1 : 0;
+            }
+            else
+            {
+                update = false;
+            }
+        }
+
+        if (update)
+        {
+            direction = checkpoint[posIndex] - checkpoint[startIndex];
+            direction = direction.normalized;
+        }
+    }
+
+    public void LoadSave(SaveMoveObject save)
+    {
+        speed = save.speed;
+        checkpoint = save.checkpoint;
+        pingPong = save.pingPong;
+
+        direction = checkpoint[1] - checkpoint[0];
+        direction = direction.normalized;
+        gameObject.transform.position = checkpoint[0];
+        gameObject.transform.GetChild(0).transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    public SaveMoveObject Save()
+    {
+        SaveMoveObject save = new SaveMoveObject();
+
+        save.speed = speed;
+        save.checkpoint = checkpoint;
+        save.pingPong = pingPong;
+
+        return save;
+    }
+}
